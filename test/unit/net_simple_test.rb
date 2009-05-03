3446dddf8ae4ff6ba80330abf75350996907cdf2
tpitale <tpitale@gmail.com>
2009-05-02 22:55:15 -0400

    initial commit

---
+++ test/unit/net_simple_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class NetSimpleTest < Test::Unit::TestCase
+
+  describe "An instance of the NetSimple class" do
+
+    it "should flunk" do
+      flunk "Please provide some tests"
+    end
+    
+  end
+
+end
